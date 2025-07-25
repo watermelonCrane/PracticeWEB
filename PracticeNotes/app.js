6af793550bc4d037a8811b0b5b1ab1aaa4fd8411
kimjaehak_laptop <kimjaehak512@gmail.com>
2025-07-25 23:09:20 +0900

    test

--- PracticeNotes/app.js
+++ PracticeNotes/app.js
@@ -1,16 +1,6 @@
-const delayedColorChange = (color, delay) => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            document.body.style.backgroundColor = color;
-            resolve();
-        }, delay);
-    })
+function test(a, b) {
+    const no = {};
+    no.a = a;
+    no.b = b;
+    console.log(this);
 }
-
-delayedColorChange('red', 1000)
-    .then(() => delayedColorChange('orange', 1000))
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('indigo', 1000));
-
