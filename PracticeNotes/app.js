cd84551c7bad0e48f494c43339844142b1cad126
Sori <ljgggg1234@gmail.com>
2025-07-25 16:24:07 +0900

    solving callback hell. using async-await

--- PracticeNotes/app.js
+++ PracticeNotes/app.js
@@ -7,10 +7,14 @@ const delayedColorChange = (color, delay) => {
     })
 }
 
-delayedColorChange('red', 1000)
-    .then(() => delayedColorChange('orange', 1000))
-    .then(() => delayedColorChange('yellow', 1000))
-    .then(() => delayedColorChange('green', 1000))
-    .then(() => delayedColorChange('blue', 1000))
-    .then(() => delayedColorChange('indigo', 1000));
+const runColorSequence = async () => {
+    await delayedColorChange('red', 1000);
+    await delayedColorChange('orange', 1000);
+    await delayedColorChange('yellow', 1000);
+    await delayedColorChange('green', 1000);
+    await delayedColorChange('blue', 1000);
+    await delayedColorChange('indigo', 1000);
+}
+
+runColorSequence();
 
